a14ea147b10d97083d98bdfe9441af05146e1f55
WIllian Paulino <willianpaulinox@gmail.com>
2022-03-08 22:49:39 -0300

    Fixed the feature which prints all equivalent word's letters. Fixed the bug of not printing all letters that are equal in the same word, found it after last commit version (29ec400ca7d8558b1f5abd80f1bd6f5ea2e9f5b0).

--- js/rodaJogo.js
+++ js/rodaJogo.js
@@ -1,19 +1,21 @@
 
-//Script da página homeForca.html
-
 const palavras = ["PEDRA", "JAZZ", "TONTO", "TELA", "CORAÇÃO", "EXCEÇÃO", "FOGÃO",
 "AMOR", "CORPO", "ADVOGADO", "CAMISETA", "COMPRA", "SERVIDOR", "FUTEBOL", "JORNAL",
 "CORRIDA", "PARALELO", "VENCEDOR", "PERDEDOR", "CORAGEM", "EMPRESA", "JAVA", "CADEIRA", 
 "BRAÇO", "ABRAÇO", "CARTÃO", "AMARELO", "BLUSA", "PARCELA", "CREDITO", "ERVA", "CALULAR", 
 "PAPEL"];
 
+
 let tela = document.querySelector('.tela-jogo');
 let pincel = tela.getContext('2d');
 pincel.lineWidth = 5; 
 pincel.strokeStyle = 'black'; 
+
 const espaco = 35;
 let contaErros = 0;
 let letraDigitada; 
+let codigoLetraDigitada;
+
 let xLetrasI = [];
 let xLetrasF = [];
 let yLetra = 370; 
@@ -27,7 +29,6 @@ botaoIniciar.addEventListener("click",function (){
 	desenhaTracosLetras(palavraForca);
 	desenhaForca();
 	desenhaLetra(palavraForca);
-	//console.log(palavraForca);
 });
 
 function iniciaJogo(palavras){
@@ -38,7 +39,7 @@ function sorteiaPalavra() {
 	let i = Math.floor(Math.random()* 10 * 3);
 	return i;
 }
-//fillRect(xi, yi, xf, yf)
+
 function desenhaTracosLetras(palavraForca) {
 	for (let i = 0; i < palavraForca.length; i++) {
 		pincel.beginPath();
@@ -83,30 +84,36 @@ function desenhaLetra(letraDigitada, xLetra, yLetra) {
 
 function checaLetra (palavraForca, xLetrasI, xLetrasF, yLetra){
 	let xDesenhaLetra = 0;
-	let contaLetra = 0;
+	letraDigitada = recebeLetraDigitada();
+	codigoLetraDigitada = recebeCodigoLetraDigitada();
 	document.querySelector('body').addEventListener('keydown', function (event){
-		if (event.keyCode >= 65 && event.keyCode <= 90) {
-			letraDigitada = event.key;
-			console.log(letraDigitada);
+		if (codigoLetraDigitada >= 65 && codigoLetraDigitada <= 90) {
 			for (let i = 0; i < palavraForca.length; i++){
-				if (letraDigitada == palavraForca[i] /*&& contaLetra === 0*/){
+				if (letraDigitada == palavraForca[i])
+				{
 					xDesenhaLetra = (xLetrasI[i] + xLetrasF[i])/2
 					desenhaLetra(letraDigitada, xDesenhaLetra, yLetra);
-					console.log(`xLetrasI antes: ${xLetrasI}`);
-					xLetrasI.splice(0, i);
-					console.log(`xLetrasI depois: ${xLetrasI}`);
-
-					//contaLetra++;
-					console.log(`xDesenhaLetra = ${xDesenhaLetra}`);
-					return;
-				}/* if (letraDigitada == palavraForca[i] && contaLetra > 0){
-					xDesenhaLetra = (xLetrasI[i] + xLetrasF[i])/2
-					desenhaLetra(letraDigitada, xDesenhaLetra, yLetra);
-				}*/ else {
+				}
+				else
+				{
 					continue;
 				}
-
 			}
 		}
 	})
 }
+
+function recebeLetraDigitada() {
+	document.querySelector('body').addEventListener('keydown', function (event){
+		letraDigitada = event.key;
+		return letraDigitada;
+	});
+}
+
+function recebeCodigoLetraDigitada (){
+	document.querySelector('body').addEventListener('keydown', function (event){
+		codigoLetraDigitada = event.keyCode;
+		return codigoLetraDigitada;
+	});
+}
+

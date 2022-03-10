05d55096cb8259aa313d526f9b9a4afde68546be
WIllian Paulino <willianpaulinox@gmail.com>
2022-03-10 13:40:49 -0300

    Changed the structure of the game. The game ran by clicking in 'novo-jogo' button. But every time that user click in 'novo-jogo' a new round started whithout ending the last round which was a serious bug. Now, the game runs when page loads and novo-jogo's button reload the page, avoiding this bug.

--- js/rodaJogo.js
+++ js/rodaJogo.js
@@ -10,33 +10,56 @@ let tela = document.querySelector('.tela-jogo');
 let pincel = tela.getContext('2d');
 pincel.lineWidth = 5; 
 pincel.strokeStyle = 'black'; 
+let areaJogo = document.querySelector('.area-jogo');
+let pagina = document.querySelector('body');
+let cabecalho = document.querySelector('.cabecalho');
+let mensagemFinalJogo = document.querySelector('.mensagem-vitoria');
+
 
 
 const espaco = 35;
 let contaErros;
-let contaAcertos
+let contaAcertos;
 let letraDigitada; 
 let codigoLetraDigitada;
-let palavraForca;
+let desenhou;
+let fimDoJogo = false; 
 
 let xLetrasI = [];
 let xLetrasF = [];
 let yLetra = 370; 
 
-	
+let xLetraErrada;
+let yLetraErrada;
 
 let botaoIniciar = document.querySelector(".novo-jogo");
-botaoIniciar.addEventListener("click",function (){
-	console.log(`palavra Forca quando clica no botão novo jogo = ${palavraForca}`);
-	console.log(typeof(palavraForca));
+botaoIniciar.addEventListener('click', function(){
+	document.location.reload(true);
+})
+
+
+rodaJogo();
+
+
+function rodaJogo (){
+	removeClasses();
+	xLetraErrada = 500;
+	yLetraErrada = 170;
+	contaAcertos = 0;
+	contaErros = 0;
+	desenhou = false;
 	pincel.clearRect(0, 0, 1200, 400);
-	palavraForca = "";
-	palavraForca = iniciaJogo(palavras);
-	console.log(`palavra Forca quando sorteiou nova palavra = ${palavraForca}`);
+
+	let palavraForca = iniciaJogo(palavras);
+	console.log(palavraForca);
 	desenhaTracosLetras(palavraForca);
 	desenhaForca();
-	desenhaLetra(palavraForca);
-});
+
+	pagina.addEventListener('keydown', function (event){
+	checaLetra(event.key, event.keyCode, palavraForca, xLetrasI, xLetrasF, yLetra);
+	});
+}
+
 
 function iniciaJogo(palavras){
 	return palavras[sorteiaPalavra()];
@@ -44,6 +67,7 @@ function iniciaJogo(palavras){
 
 function sorteiaPalavra() {
 	let i = Math.floor(Math.random()* 10 * 3);
+	console.log(i);
 	return i;
 }
 
@@ -55,15 +79,7 @@ function desenhaTracosLetras(palavraForca) {
 		pincel.moveTo(xLetrasI[i], 380);
 		pincel.lineTo(xLetrasF[i], 380);
 		pincel.stroke();
-		console.log(palavraForca);
-		console.log(`
-		X inicial da ${i}a letra: ${xLetrasI[i]}
-		X final da ${i}a letra: ${xLetrasF[i]}
-		`);
 	}
-	console.log(xLetrasI);
-	console.log(xLetrasF);
-	checaLetra(palavraForca, xLetrasI, xLetrasF, yLetra);
 }
 
 function desenhaForca() {
@@ -81,60 +97,110 @@ function desenhaForca() {
 		pincel.stroke();
 }
 
+function desenhaBoneco (contaErros) {
+	raio = 40;
+	pincel.fillStyle = 'black';
+	pincel.beginPath();
+	pincel.arc(270, 100+raio, raio, 0, 2 * Math.PI);
+	pincel.stroke();
+	if (contaErros == 2) {
+		pincel.beginPath();
+		pincel.moveTo(270, 100+2*raio);
+		pincel.lineTo(330, 160+2*raio);
+		pincel.stroke();
+	}
+	if (contaErros == 3) {
+		pincel.beginPath();
+		pincel.moveTo(270, 100+2*raio);
+		pincel.lineTo(210, 160+2*raio);
+		pincel.stroke();
+	}
+	if (contaErros == 4) {
+		pincel.beginPath();
+		pincel.moveTo(270, 100+2*raio);
+		pincel.lineTo(270, 220+2*raio);
+		pincel.stroke();
+	}
+	if (contaErros == 5) {
+		pincel.beginPath();
+		pincel.moveTo(270, 220+2*raio);
+		pincel.lineTo(200, 280+2*raio);
+		pincel.stroke();
+	}
+	if (contaErros == 6) {
+		pincel.beginPath();
+		pincel.moveTo(270, 220+2*raio);
+		pincel.lineTo(340, 280+2*raio);
+		pincel.stroke();
+	}
+}
+
 function desenhaLetra(letraDigitada, xLetra, yLetra) {
 	pincel.font = "40px Arial";
 	pincel.textBaseline = 'alphabetic';
 	pincel.fillText(letraDigitada, xLetra, yLetra);
-	console.log(xLetra);
+}
+
+function desenhaLetraErrada(letraDigitada){
+	pincel.font = "40px Arial";
+	pincel.textBaseline = 'alphabetic';
+	pincel.fillText(letraDigitada, xLetraErrada, yLetraErrada);
+	xLetraErrada += 60;
 }
 
 
-function checaLetra (palavraForca, xLetrasI, xLetrasF, yLetra){
-	letraDigitada = recebeLetraDigitada();
-	codigoLetraDigitada = recebeCodigoLetraDigitada();
-	contaAcertos = 0;
-
-	document.querySelector('body').addEventListener('keydown', function (){
-		let xDesenhaLetra = 0;
-
-		if (codigoLetraDigitada >= 65 && codigoLetraDigitada <= 90) {
-			for (let i = 0; i < palavraForca.length; i++){
-				if (letraDigitada == palavraForca[i])
-				{
-					xDesenhaLetra = ((xLetrasI[i] + xLetrasF[i])/2)-12;
-					desenhaLetra(letraDigitada, xDesenhaLetra, yLetra);
-					contaAcertos++;
-					checaVitoria(contaAcertos, palavraForca);
-				}
-				else
-				{	
-					continue;
-					//contaErros++;
-				}
+function checaLetra (letraDigitada, codigoLetra, palavra, xLetrasI, xLetrasF, yLetra){
+	let xDesenhaLetra = 0;
+	desenhou = false;
+	if (codigoLetra >= 65 && codigoLetra<= 90) {
+		for (let i = 0; i < palavra.length; i++){
+			if (letraDigitada == palavra[i])
+			{	
+				xDesenhaLetra = ((xLetrasI[i] + xLetrasF[i])/2)-12;
+				desenhaLetra(letraDigitada, xDesenhaLetra, yLetra);
+				contaAcertos++;
+				checaVitoria(contaAcertos, palavra);
+				desenhou = true;
 			}
 		}
-		botaoIniciar.addEventListener('click', function(){
-			palavraForca = "";
-		})
-	})
+		if (desenhou == false && palavra.length != 0)
+		{
+			desenhou = true;
+			contaErros++;
+			checaDerrota(contaErros);
+			desenhaBoneco(contaErros);
+			desenhaLetraErrada(letraDigitada);
+		}
+	}
 }
 
-function recebeLetraDigitada() {
-	document.querySelector('body').addEventListener('keydown', function (event){
-		letraDigitada = event.key;
-		return letraDigitada;
-	});
-}
-
-function recebeCodigoLetraDigitada (){
-	document.querySelector('body').addEventListener('keydown', function (event){
-		codigoLetraDigitada = event.keyCode;
-		return codigoLetraDigitada;
-	});
-}
 
 function checaVitoria (contaAcertos, palavraForca) {
 	if (contaAcertos == palavraForca.length){
-		console.log("Você venceu!!");
+		exibeMensagemFimDoJogo("voce-venceu", "mensagem-vitoria","Você Venceu! Parabéns");
+		palavraForca = "";
 	}
+}
+
+function checaDerrota (contaErros){
+	if (contaErros == 6){
+		exibeMensagemFimDoJogo("voce-perdeu", "mensagem-derrota", "Você Perdeu!");
+	}
+}
+
+function exibeMensagemFimDoJogo(classeDesfecho, classeMensagem, mensagem){
+	areaJogo.classList.add(classeDesfecho);
+	mensagemFinalJogo.textContent = mensagem;
+	mensagemFinalJogo.classList.remove("invisible");
+	mensagemFinalJogo.classList.add(classeMensagem);
+	fimDoJogo = true;
+}
+
+function removeClasses(){
+	areaJogo.classList.remove('voce-venceu');
+	areaJogo.classList.remove('voce-perdeu');
+	mensagemFinalJogo.classList.add('invisible');
+	mensagemFinalJogo.classList.remove('mensagem-vitoria');
+	mensagemFinalJogo.classList.remove('mensagem-derrota');
+	mensagemFinalJogo.innerHTML = "";
 }

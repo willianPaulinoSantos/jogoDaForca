bec4e5612ac6eb6812cf9c63b3224d5e3ee39532
Willian Paulino <93926613+willianPaulinoSantos@users.noreply.github.com>
2022-03-13 10:04:12 -0300

    Update jogoForca.js
    Solving conflicts

--- js/jogoForca.js
+++ js/jogoForca.js
@@ -1,7 +1,7 @@
 import rodaJogo from "../scripts/rodaJogo.js";
 
 
-<<<<<<< HEAD
+
 
 
 
@@ -10,8 +10,8 @@ let pincel = tela.getContext('2d');
 pincel.lineWidth = 5; 
 pincel.strokeStyle = 'black'; 
 let areaJogo = document.querySelector('.area-jogo');
-=======
->>>>>>> 2a3b5cb6fbcb8595c1332de5933c2a4e89e59e47
+
+
 let pagina = document.querySelector('body');
 let areaJogo = document.querySelector('.area-jogo');
 let mensagemFinalJogo = document.querySelector('.mensagem-vitoria');
